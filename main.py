b9502830f42092690fc64c58ca2e4b03eb0a0610
EVGENII SHIROKOV <>
2021-04-17 20:49:37 +0000

    + added categories of BMI which are depending on your BMI

--- main.py
+++ main.py
@@ -8,10 +8,17 @@ weight = input("enter your weight in kg: ")
 
 bmi = int(weight) / float(height) ** 2
 bmi_as_int = round(bmi, 2)
-print(f"Your BMI is: {bmi_as_int}")
 
-
-
+if bmi < 18.5:
+  print(f"Your BMI is: {bmi_as_int}, you are underweight.")
+elif bmi < 25:
+  print(f"Your BMI is: {bmi_as_int}, you are a normal weight.")
+elif bmi < 30:
+  print(f"Your BMI is: {bmi_as_int}, you are overweight.")
+elif bmi < 35:
+  print(f"Your BMI is: {bmi_as_int}, you are obese.")
+else:
+  print(f"Your BMI is: {bmi_as_int}, you are clinically obese.")
 
 
 
